ad4d9af2eb8ddf6afa31ec5198053bfc904e619f
John O'Meara <john.fr.omeara@gmail.com>
2016-06-07 18:08:29 -0400

    Move code processing to common function. Make debug statements flagged.
    This cleans up some of the processing, especially in the connection phase, and
    also now captures capability changes resulting from login that the server
    provices for us.
    
    The debug info can still be useful, but isn't often needed and clutters the
    screen. Making it depend on a debug variable cleans it up for now. Eventually,
    it should be calling some kind of logging function with a debug level so
    filtering can be done by the logger instead of us.
    
    Also added is processing for untagged data without an index number prefix
    (e.g. untagged FLAGS response).
    
    At this point, connecting to local dovecot is clean.

--- experiments/email-test.py
+++ experiments/email-test.py
@@ -7,6 +7,62 @@ import tempfile
 import os
 import sys
 
+# Some notes:
+#
+# RFC 822, then 2822, spec the message format.
+# 2822 states that there are only 2 header fields that MUST be in every
+# message:
+#
+#   From:
+#   Date:
+#
+# The "Date" header indicates when the sender queues the message for delivery
+# (when the message is in its final form). It is not when it actually enters
+# transit (e.g. an offline user may type an email and hit send or enqueue or
+# whatever, then connect to the network an hour later. The earlier time goes
+# into the Date field)
+#
+# The 'From' header specifies the author or responsible entity for writing the
+# message. Related fields include 'Sender', which specifies the entity
+# responsible for transmitting the message (e.g. a secretary might 'send' a
+# message that was 'written' by his boss). Sender should not be specified if
+# identical to From. Another related field is 'Reply-To', which indicates that
+# replies should not go to the mailbox specified by 'From'.
+#
+# Other interesting notes:
+#   * Bcc header can be used in 4 ways:
+#       * Stripped after composing a message prior to sending to recipients
+#       * Stripped to non Bcc recipients but kept for Bcc recipients (2 actual
+#         messages sent on-the-wire)
+#       * Stripped prior to sending, but added to Bcc recipients with just
+#         their name (1 regular message + n Bcc messages on-the-wire)
+#       * Stripped of content before sending, indicating that one or more
+#         unnamed persons received a copy. This method may actually be
+#         combined with methods 2 or 3 for the primary receivers.
+#     For composing mail, perhaps these should be presented as options.
+#     For viewing mail, should show a flag or something for Bcc that is empty.
+#     When replying, the replier may leak information if they were in the Bcc
+#     list. 2822 notes that each of the above Bcc stripping methods has
+#     varying security concerns. (see section 5)
+#   * Replies should move "To" entities to the Cc header, except for the
+#     person being replied to. The spec does mention that a application may
+#     duplicate the destination addresses for the response, but says it
+#     doesn't address the implications of doing this. We ought to allow either
+#     way. In particular in some office contexts, the To list is used for
+#     those active in the discussion, and the Cc list is for people who should
+#     be aware of, but not participating in, the discussion (e.g. Engineers on
+#     the To field and Managers in the Cc field, when dealing with a low-level
+#     technical problem. The Managers don't need to make decisions (until they
+#     do), but need to be aware that a problem is being worked upon).
+#
+# Beyond the required headers, there is a list of SHOULD headers:
+#
+#   * Message-ID
+#   * In-Reply-To (where relevent)
+#   * References (where relevant)
+#
+#
+
 f=tempfile.mkstemp()
 res = os.system("vim %s" % f[1])
 if (res != 0):
