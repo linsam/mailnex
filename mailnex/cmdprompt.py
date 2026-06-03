linsam/mailnex

commit 8e89424b3b4f70e64ea562dbd05a2f8bc7ee1dd8

Fix using filter programs (e.g. 'pipe-text/html' setting)

mailnex/cmdprompt.py
@@ -311,46 +311,18 @@ class CmdPrompt(cmd.Cmd):
         result = await anyio.run_process(command=args, input=data, check=False, stdout=None, stderr=None)
         self.ttyBusy = False
         return result.returncode
-    def runAProgramAsFilter(self, args, data):
+    async def runAProgramAsFilter(self, args, data):
         """Run a program with the given input. Return the output. Leaves stderr alone.
 
-        This should be run when the prompt is inactive."""
-        res=[None, b""]
-        done=[0]
-        def finish(proc,status,signal):
-            proc.close()
-            proc.loop.stop()
-            res[0] = status
-        com = uvloop.Pipe(self.ptkevloop.realloop, True)
-        como = uvloop.Pipe(self.ptkevloop.realloop, True)
-        stdio = [
-                uvloop.StdIO(stream=com, flags=uvloop.UV_CREATE_PIPE | uvloop.UV_READABLE_PIPE),
-                uvloop.StdIO(stream=como, flags=uvloop.UV_CREATE_PIPE | uvloop.UV_WRITABLE_PIPE),
-                uvloop.StdIO(fd=sys.stderr.fileno(), flags=uvloop.UV_INHERIT_FD),
-                ]
+        This should be run when the prompt is inactive.
+
+        returns a tuple of return code and stdout data.
+	"""
         self.ttyBusy = True
-        s = uvloop.Process.spawn(self.ptkevloop.realloop, args, stdio=stdio, exit_callback=finish)
-        def closeWhenDone(handle, error):
-            # TODO: Maybe report error?
-            handle.close()
-        def doneWriting(handle, error):
-            # TODO: Maybe report error?
-            if done[0]:
-                closeWhenDone(handle, error)
-            done[0] = True
-        def readCb(handle, data, error):
-            if data is None:
-                handle.stop_read()
-                if done[0]:
-                    closeWhenDone(handle, error)
-                done[0] = True
-                return
-            res[1] += data
-        como.start_read(readCb)
-        com.write(data, closeWhenDone)
-        self.ptkevloop.realloop.run()
+        result = await anyio.run_process(command=args, input=data, check=False, stderr=None)
+        breakpoint()
         self.ttyBusy = False
-        return res
+        return (result.returncode, result.stdout)
     async def runAProgramStraight(self, args):
         """Run a program without anything special. Leaves stdin/stdout/stderr alone.
 

mailnex/mailnex.py
@@ -4109,7 +4109,7 @@ class Cmd(cmdprompt.CmdPrompt):
         headerstr += '\r\n'
         return headerstr
 
-    def partsToString(self, parts, allHeaders=False):
+    async def partsToString(self, parts, allHeaders=False):
         # parts[0] is None or 'info' or the tag of the message part
         # parts[1] is None or 'header' or the struct object for the message
         # part
@@ -4198,10 +4198,10 @@ class Cmd(cmdprompt.CmdPrompt):
                             outfile.write(encdata)
                             outfile.flush()
                             cmd = printfStyle.replace(cmd, {'s': s, 't': t, 'f': outfile.name})
-                            status, data = self.runAProgramAsFilter(['/bin/sh', '-c', cmd], b"")
+                            status, data = await self.runAProgramAsFilter(['/bin/sh', '-c', cmd], b"")
                     else:
                         cmd = printfStyle.replace(cmd, {'s': s, 't': t})
-                        status, data = self.runAProgramAsFilter(['/bin/sh', '-c', cmd], encdata)
+                        status, data = await self.runAProgramAsFilter(['/bin/sh', '-c', cmd], encdata)
                     if status == 0:
                         oenc = None
                         settingsearch = [
@@ -4672,7 +4672,7 @@ class Cmd(cmdprompt.CmdPrompt):
         parts = self.getTextPlainParts(index)
         # TODO: This code copied from do_print.
         # Should be made common. See also TODOs from there.
-        body = self.partsToString(parts)
+        body = await self.partsToString(parts)
         content = b"\033[7mMessage %i:\033[0m\n" % index
         content += body.encode('utf-8')
         res = await self.runAProgramWithInput(["less","-R"], content)
@@ -4776,7 +4776,7 @@ class Cmd(cmdprompt.CmdPrompt):
         if len(parts) < 2:
             print("Message has no displayable parts")
             return
-        body = self.partsToString(parts)
+        body = await self.partsToString(parts)
 
         # TODO: Use terminfo/termcap (or perhaps pygments or prompt_toolkit)
         # for styling
@@ -4847,7 +4847,7 @@ class Cmd(cmdprompt.CmdPrompt):
         if len(parts) < 2:
             print("Message has no displayable parts")
             return
-        body = self.partsToString(parts, True)
+        body = await self.partsToString(parts, True)
 
         # TODO: Use terminfo/termcap (or perhaps pygments or prompt_toolkit)
         # for styling

commit 72ef4bdbc1b170f436fe3504b236ea99570f8327

Fix erroneous breakpoint in filter
Introduced in 8e89424b3b4f70e64ea562dbd05a2f8bc7ee1dd8

mailnex/cmdprompt.py
@@ -331,7 +331,6 @@ class CmdPrompt(cmd.Cmd):
 	"""
         self.ttyBusy = True
         result = await anyio.run_process(command=args, input=data, check=False, stderr=None)
-        breakpoint()
         self.ttyBusy = False
         return (result.returncode, result.stdout)
     async def runAProgramStraight(self, args):